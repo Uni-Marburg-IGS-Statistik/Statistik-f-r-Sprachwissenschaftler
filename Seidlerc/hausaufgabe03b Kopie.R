db48f7fd31952f62b26f57230ea67b83bf7309bf
Seidlercarmen <Seidlerc@students.uni-marburg.de>
2014-04-24 16:57:06 +0200

    Name, Mail, Datum geändert und aktualisiert, Lizenz unverändert

--- Seidlerc/hausaufgabe03b Kopie.R
+++ Seidlerc/hausaufgabe03b Kopie.R
@@ -1,6 +1,6 @@
 # Hausaufgabe 03
-# Phillip Alday <phillip.alday@staff.uni-marburg.de>
-# 2014-04-23
+# Carmen Seidler <seidlerc@students.uni-marburg.de>
+# 2014-04-24
 # Dieses Werk ist lizenziert unter einer CC-BY-NC-SA Lizenz.
 
 # Sie sollten die Datei auch in Ihren Ordner kopieren und einen Commit machen, 
