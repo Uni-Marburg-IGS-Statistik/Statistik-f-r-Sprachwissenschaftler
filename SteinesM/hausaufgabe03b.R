12ebc3616b5118fbbe18edb0e23dd5c3006bab4a
SteinesM <Steines@students.uni-marburg.de>
2014-04-25 19:06:10 +0200

    Lizenz

--- SteinesM/hausaufgabe03b.R
+++ SteinesM/hausaufgabe03b.R
@@ -1,7 +1,7 @@
 # Hausaufgabe 03
 # Miriam Steines <Steines@students.uni-marburg.de>
 # 2014-04-25
-# Dieses Werk ist lizenziert unter einer CC-BY-NC-SA Lizenz.
+# Diese Datei darf weiter als Beispiel genutzt werden.
 
 # Sie sollten die Datei auch in Ihren Ordner kopieren und einen Commit machen, 
 # bevor Sie die Kopie weiter anpassen! Vergessen Sie dabei nicht, Namen, Datum
