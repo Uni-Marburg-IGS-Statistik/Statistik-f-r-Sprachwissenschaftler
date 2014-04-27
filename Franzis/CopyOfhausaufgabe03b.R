31996138ab44d84405211a3147626d9ce1ab298b
Isabelle <Franzis@students.uni-marburg.de>
2014-04-27 13:11:30 +0200

    Franzis_Ha3b_fertig_mit_Lizenz

--- Franzis/CopyOfhausaufgabe03b.R
+++ Franzis/CopyOfhausaufgabe03b.R
@@ -1,7 +1,7 @@
 # Hausaufgabe 03
 # Isabelle Franz <Franzis@students.uni-marburg.de>
 # 2014-04-25
-# Dieses Werk ist lizenziert unter einer CC-BY-NC-SA Lizenz.
+# Darf nur zu Prüfungszwecken verwendet werden
 
 # Sie sollten die Datei auch in Ihren Ordner kopieren und einen Commit machen, 
 # bevor Sie die Kopie weiter anpassen! Vergessen Sie dabei nicht, Namen, Datum
