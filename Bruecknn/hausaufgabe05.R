4ed3050d241387099b9c06e7bbb70fe3f79fc9eb
Nadine Brückner <bruecknn@students.uni-marburg.de>
2014-05-01 13:54:07 +0200

    Code aktiv gesetzt

--- Bruecknn/hausaufgabe05.R
+++ Bruecknn/hausaufgabe05.R
@@ -1,6 +1,6 @@
 # Hausaufgabe 04
-# Phillip Alday <phillip.alday@staff.uni-marburg.de>
-# 2014-04-30
+# Nadine Brückner <Bruecknn@students.uni-marburg.de>
+# 2014-05-01
 # Dieses Werk ist lizenziert unter einer CC-BY-NC-SA Lizenz.
 
 # Sie sollten die Datei auch in Ihren Ordner kopieren und einen Commit machen, 
@@ -30,6 +30,8 @@ dat <- read.table("body_dim_long.tab",header=TRUE)
 # print() einpacken.
 #weight.grafik <- ggplot(data=dat,aes(x=weight)) + geom_histogram(aes(y=..density..),fill="white",color="black") + geom_density()
 #print(weight.grafik)
+weight.grafik <- ggplot(data=dat,aes(x=weight)) + geom_histogram(aes(y=..density..),fill="white",color="black") + geom_density()
+print(weight.grafik)
 
 # Wenn wir verschiedene Grafiken mit einem Datenzsatz machen möchten, ist es
 # nervig, wenn wir den gemeinsamen Teil immer wieder eingeben müssen. Auch Teile
@@ -37,16 +39,23 @@ dat <- read.table("body_dim_long.tab",header=TRUE)
 #weight.grafik.basis <- ggplot(data=dat,aes(x=weight))
 #print(weight.grafik.basis + geom_histogram())
 #print(weight.grafik.basis + geom_density())
+weight.grafik.basis <- ggplot(data=dat,aes(x=weight))
+print(weight.grafik.basis + geom_histogram())
+print(weight.grafik.basis + geom_density())
 
 # Wir haben auch mal die qplot()-Funktion gesehen. Sie ergibt eigentlich das
 # Gleiche wie die "ggplot() + ..."-Befehle, hat nun eine andere Schnittstelle. 
 #weight.grafik.alt <- qplot(x=weight,data=dat,geom="density")
 #print(weight.grafik.alt)
+weight.grafik.alt <- qplot(x=weight,data=dat,geom="density")
+print(weight.grafik.alt)
 
 # Weil das Gleiche ergeben wird, können wir auch den Ouput von qplot() mit
 # weiteren geom_XXXX() Funktionen erweitern. 
 #weight.grafik.alt2 <- weight.grafik.alt + geom_histogram(aes(y=..density..),fill="white",color="black")
 #print(weight.grafik.alt2)
+weight.grafik.alt2 <- weight.grafik.alt + geom_histogram(aes(y=..density..),fill="white",color="black")
+print(weight.grafik.alt2)
 
 # Sie sehen an dieser Grafik auch, dass ggplot gestappelte Layers nutzt -- das 
 # Histogramm wird auf das Layer mit Dichte gestappelt und daher wird die 
@@ -54,24 +63,33 @@ dat <- read.table("body_dim_long.tab",header=TRUE)
 # transparenter machen.
 #weight.grafik.alt3 <- weight.grafik.alt + geom_histogram(aes(y=..density..),fill="white",color="black",alpha=0.65)
 #print(weight.grafik.alt3)
+weight.grafik.alt3 <- weight.grafik.alt + geom_histogram(aes(y=..density..),fill="white",color="black",alpha=0.65)
+print(weight.grafik.alt3)
 
 # ggplot hat auch eingebaute Untestützung für Box-Whisker-Plots, allerdings sind x und y jetzt anders:
 #weight.bw <- weight.grafik.basis + geom_boxplot(aes(x="weight",y=weight))
 #print(weight.bw)
+weight.bw <- weight.grafik.basis + geom_boxplot(aes(x="weight",y=weight))
+print(weight.bw)
 # Sie sehen auch dabei, dass Layer-Asthetics Basis-Athetics brechen. 
 
 # Aber viel interessanter ist eben, wenn wir Gruppen unterscheiden. Dann können etwas machen wie BW-Plot nach Geschlecht:
 #weight.bw.sex <- weight.grafik.basis + geom_boxplot(aes(x=sex,y=weight))
 #print(weight.bw.sex)
+weight.bw.sex <- weight.grafik.basis + geom_boxplot(aes(x=sex,y=weight))
+print(weight.bw.sex)
 
 # Und als Erinnerung können wir auch ähnliches mit der Dichte machen:
 #print( weight.grafik.basis + geom_density(aes(color=sex,fill=sex),alpha=0.5) )
+print( weight.grafik.basis + geom_density(aes(color=sex,fill=sex),alpha=0.5) )
+
 
 # Aber jetzt haben wir uns Gewicht mehrmals angeschaut. Es wird Zeit, dass uns
 # auch Größe anschauen. Sind die Studenten mancher Studiengänge größer als die anderen?
 # Weil wir deutlich weniger Männer haben und es einen bekannten Unterschied in der Größe 
 # zwischen Männern und Frauen gibt, schließen wir erstmal die Männer aus:
 #frauen <- subset(dat, CODE_HIER)
+frauen <- subset(dat, CODE_HIER)
 
 # (Sie sollten sich wirklich überlegen, ob der Schritt "gut" ist. Haben wir 
 # dadurch unsre Ergebnisse verstellt? Sie müssen hier nichts schreiben, aber 
@@ -94,6 +112,8 @@ dat <- read.table("body_dim_long.tab",header=TRUE)
 # Wir können natürlich auch die Dichte anschauen:
 #frauen.studiengang.dichte <- CODE_HIER
 #print(frauen.studiengang.dichte)
+frauen.studiengang.dichte <- CODE_HIER
+print(frauen.studiengang.dichte)
 
 # Haben Sie den gleichen Eindruck wie bei Box-Whisker bekommen? Unterscheiden
 # sich die Gruppen?
@@ -108,6 +128,9 @@ dat <- read.table("body_dim_long.tab",header=TRUE)
 # einer weiteren Syntax machen:
 #klinisch <- frauen[frauen$major == "M.A..Klinische.Linguistik",]
 #print(klinisch)
+klinisch <- frauen[frauen$major == "M.A..Klinische.Linguistik",]
+print(klinisch)
+
 
 # Das sieht erstmal sehr vervwirrend aus, ist es aber nicht. Die eckigen
 # Klammern bestimmen die Auswahl an Elementen. Wir haben das ja bei Indizen in
@@ -125,6 +148,8 @@ dat <- read.table("body_dim_long.tab",header=TRUE)
 # HINT: wie sehen die Namen aus bzw. wie werden sie im data frame buchstabiert?
 #linkk <- frauen[CODE_HIER]
 #speech <- frauen[CODE_HIER] 
+linkk <- frauen[CODE_HIER]
+speech <- frauen[CODE_HIER]
 
 # Berechnen Sie -- ohne Hilfe von sd() -- die Standardabweichung für die Größe der drei 
 # Gruppen. Sie können auch weitere Zeilen hinzufügen, wenn es Ihnen so leichter
@@ -133,6 +158,9 @@ dat <- read.table("body_dim_long.tab",header=TRUE)
 #klinisch.sd <- CODE_HIER
 #linkk.sd <- CODE_HIER
 #speech.sd <- CODE_HIER
+klinisch.sd <- CODE_HIER
+linkk.sd <- CODE_HIER
+speech.sd <- CODE_HIER
 
 # Berichten Sie jetzt die Mittelwerte und Standardabweichungen für die drei Gruppen. Die erste Gruppe steht hier als Muster:
 #print( paste("Studiengang: Klinische Linguistik","Mean:",mean(klinisch$height),"SD:",klinisch.sd) )
