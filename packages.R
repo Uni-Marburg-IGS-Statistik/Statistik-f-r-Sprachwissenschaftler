6c599796fe826f2f8ff0a42bfdda7d0277148a70
Phillip Alday <phillip.alday@staff.uni-marburg.de>
2014-05-07 16:48:50 +0200

    updated package list

--- packages.R
+++ packages.R
@@ -2,4 +2,8 @@ packages.used <- c("knitr","knitcitations"
                    ,"ggplot2"
                    ,"plyr"
                    ,"languageR"
+                   ,"reshape2"
+                   ,"reshape"
                    )
+
+install.packages(packages.used,dependencies = TRUE)
