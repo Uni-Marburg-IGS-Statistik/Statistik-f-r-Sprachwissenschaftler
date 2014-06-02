c3a26b516f1284a8faafdd8f065b314e3cd44a46
Phillip Alday <phillip.alday@staff.uni-marburg.de>
2014-06-02 11:10:09 +0200

    explicity require markdown package for shinyapps deployment

--- Shiny/confidence_intervals/ui.R
+++ Shiny/confidence_intervals/ui.R
@@ -16,6 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 library(shiny)
+require(markdown)
 
 shinyUI(fluidPage(
   titlePanel("Confidence Intervals"),
--- Shiny/optional_stopping/ui.R
+++ Shiny/optional_stopping/ui.R
@@ -16,6 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 library(shiny)
+require(markdown)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
