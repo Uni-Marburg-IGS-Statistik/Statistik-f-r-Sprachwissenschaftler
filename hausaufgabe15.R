30a039bda44583b2c5846f87b1221baa35d18a03
Isabelle <Franzis@students.uni-marburg.de>
2014-06-05 14:24:56 +0200

    Ha 15

--- hausaufgabe15.R
+++ hausaufgabe15.R
@@ -1,6 +1,6 @@
 # Hausaufgabe 15
-# Phillip Alday <phillip.alday@staff.uni-marburg.de>
-# 2014-06-04
+# Isabelle Franz <franzis@students.uni-marburg.de>
+# 2014-06-05
 # Dieses Werk ist lizenziert unter einer CC-BY-NC-SA Lizenz.
 
 
